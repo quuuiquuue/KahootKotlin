566ced4c4a5833a66dabd841ad95e349fb1b1ea4
quuuiquuue <econlop014@g.educaand.es>
2024-11-19 10:34:05 +0100

    Activity_main completed

--- app/src/main/java/com/example/kahoot/MainActivity.kt
+++ app/src/main/java/com/example/kahoot/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.kahoot
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
         val btnConfig = findViewById<Button>(R.id.btnConfig)
